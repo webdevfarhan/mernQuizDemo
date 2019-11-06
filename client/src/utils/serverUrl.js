59ac22fa8bdbbda1d4fe81a1af65148d58bea7c7
Farhan <frhnmdni@gmail.com>
2019-11-06 12:33:11 +0530

    change server urls

--- client/src/utils/serverUrl.js
+++ client/src/utils/serverUrl.js
@@ -1,3 +1,3 @@
 let serverUrl;
-process.env.REACT_APP_SERVER_URL === 'localhost' ? (serverUrl = 'http://localhost:5000') : (serverUrl = 'https://appslel.com');
+process.env.REACT_APP_SERVER_URL === 'localhost' ? (serverUrl = 'http://localhost:5001') : (serverUrl = 'https://test.appslel.com');
 export default serverUrl;
